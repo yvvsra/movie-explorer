8d09cc8b478487b565f2b29dcf08cf8fff196c36
naderalfakesh <nader.alfakesh@gmail.com>
2020-03-12 20:30:54 +0300

    ConstrucrtURL

--- src/App.js
+++ src/App.js
@@ -11,12 +11,25 @@ class App extends React.Component {
       query: ""
     }
   }
+  constructURL = ()=>{
+    let URL= ""
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_KEY = "542003918769df50083a13c415bbc602";
+    URL = BASE_URL;
+    if(this.state.query === ""){
+      URL += `/movie/popular?api_key=${API_KEY}`
+    }
+    else{
+      URL += `/search/movie?api_key=${API_KEY}&query=${this.state.query}&include_adult=false`
+    }
+    console.log(URL)
+    return URL
+  }
 
-   BASE_URL = "https://api.themoviedb.org/3";
-   API_KEY = "542003918769df50083a13c415bbc602";
+   
 
    componentDidMount = () => {
-    fetch(`${this.BASE_URL}/movie/popular?api_key=${this.API_KEY}`)
+    fetch(this.constructURL())
     .then(resp => resp.json())
     .then(json=> {
       this.setState({
@@ -32,7 +45,7 @@ class App extends React.Component {
    }
    handlesubmit= (e)=>{
      if(e.key ==="Enter"){
-      fetch(`${this.BASE_URL}/search/movie?api_key=${this.API_KEY}&query=${this.state.query}&include_adult=false`)
+      fetch(this.constructURL())
       .then(resp => resp.json())
       .then(json=> {
         this.setState({
