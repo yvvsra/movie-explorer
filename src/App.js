26118949b37af25bc0e216aa287429fb44260af0
lizbur10 <liz.burton147@gmail.com>
2020-03-12 19:53:34 +0300

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import movies from './data';
+// import movies from './data';
 import MoviesList from "./Components/MovieList/index.js"
 import Header from "./Components/Header/index.js"
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+   BASE_URL = "https://api.themoviedb.org/3";
+   API_KEY = "542003918769df50083a13c415bbc602";
+
+   componentDidMount = () => {
+    fetch(`${this.BASE_URL}/movie/popular?api_key=${this.API_KEY}`)
+    .then(resp => resp.json())
+    .then(json=> {
+      this.setState({
+        movies: json.results
+      }, () => console.log(this.state))
+    })
+   }
+
   render() {
     return (
       <div>
           <Header />
-          <MoviesList movies={movies.results} />        
+          <MoviesList movies={this.state.movies} />        
       </div>
     );
   }
